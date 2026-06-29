7f3c962d5fbf6ba1403645efd94820dfc1fc74f0
Josh Lessard <josh.lessard@gmail.com>
2026-06-29 17:13:21 -0400

    Fix test context load failure by inserting a test Keycloak container instead of allowing auto-OIDC invocation of issuer URI.

--- src/test/java/com/polarbookshop/edgeservice/EdgeServiceApplicationTests.java
+++ src/test/java/com/polarbookshop/edgeservice/EdgeServiceApplicationTests.java
@@ -5,6 +5,7 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.DynamicPropertyRegistry;
 import org.springframework.test.context.DynamicPropertySource;
 import org.testcontainers.containers.GenericContainer;
+import org.testcontainers.containers.wait.strategy.Wait;
 import org.testcontainers.junit.jupiter.Container;
 import org.testcontainers.junit.jupiter.Testcontainers;
 import org.testcontainers.utility.DockerImageName;
@@ -16,6 +17,7 @@ import org.testcontainers.utility.DockerImageName;
 class EdgeServiceApplicationTests {
 
 	private static final int REDIS_PORT = 6379;
+	private static final int KEYCLOAK_PORT = 8080;
 
 	@Container
 	static GenericContainer<?> redis = new GenericContainer<>( DockerImageName.parse( "redis:7.0" ) )
@@ -27,8 +29,25 @@ class EdgeServiceApplicationTests {
 		registry.add( "spring.data.redis.port", () -> redis.getMappedPort( REDIS_PORT ) );
 	}
 
+	@Container
+	static final GenericContainer<?> keycloak = new GenericContainer<>( DockerImageName.parse( "quay.io/keycloak/keycloak:26.1" ) )
+		.withExposedPorts( KEYCLOAK_PORT )
+		// Instructs Keycloak to create a realm and client via CLI variables on start
+		.withCommand( "start-dev" )
+		.waitingFor( Wait.forHttp( "/" ).forPort( KEYCLOAK_PORT ) );
+
+	@DynamicPropertySource
+	static void oidcProperties( DynamicPropertyRegistry registry ) {
+		String oidcIssuerUrl = String.format(
+			"http://%s:%d/realms/master",
+			keycloak.getHost(),
+			keycloak.getMappedPort( KEYCLOAK_PORT )
+		);
+
+		registry.add( "spring.security.oauth2.client.provider.keycloak.issuer-uri", () -> oidcIssuerUrl );
+	}
+
 	@Test
 	void contextLoads() {
 	}
-
 }
